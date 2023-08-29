d2a8232cf85bf89622c2d376f75f6282255e0146
ThanyawitNiti <thanyawit.nitipornwattana@gmail.com>
2023-08-29 23:04:48 +0700

    Edit

---
+++ 02-String/01/main.js
@@ -0,0 +1,3 @@
+// จงสร้างตัวแปรและกำหนดค่าเริ่มต้นให้ตัวแปรมีค่า 'c:\Users\Admin\Desktop'
+
+let variable = 'c:\Users\Admin\Desktop';
---
+++ 02-String/02/main.js
@@ -0,0 +1,17 @@
+
+// จงหาความยาวของตัวแปร String ต่อไปนี้
+// - 'Welcome to Thailand'
+// - 'Don\'n be afraid\n'
+// - 'สวัสดีประเทศไทย'
+
+let a = 'Welcome to Thailand'
+let al = a.length
+console.log(al)
+
+let b = 'Don\'n be afraid\n'
+let bl = b.length
+console.log(bl)
+
+let c = 'สวัสดีประเทศไทย'
+let cl = c.length
+console.log(cl)
